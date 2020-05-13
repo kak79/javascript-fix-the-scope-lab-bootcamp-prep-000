2bcf66f4e579352903d2d27f0faf700e4aeb0d83
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-13 17:21:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function yourAnimal() {
   // However, you cannot simply modify the existing variable declared on line 1 in the global scope 
   // How can we make sure that this function and the above function both pass?
   // 
-  // P.S.: Hard-coding 'cat' below will not work
+  // 
   return animal
 }
 
